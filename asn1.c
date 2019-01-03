011d3f4a58f766188aa0d93759211cdb6edd923a
Andrea Brancaleoni <miwaxe@gmail.com>
2019-01-03 09:07:55 -0800

    Remove CRYPTO_free_ex_index usage in fuzz tests.
    This function was used to generate a fault and unlock the global ex_data RW lock.
    Since that lock does not exist in libressl it can be safely replaced with a NOP.

--- asn1.c
+++ asn1.c
@@ -260,7 +260,6 @@ int FuzzerInitialize(int *argc, char ***argv)
     OPENSSL_init_crypto(OPENSSL_INIT_LOAD_CRYPTO_STRINGS, NULL);
     OPENSSL_init_ssl(OPENSSL_INIT_LOAD_SSL_STRINGS, NULL);
     ERR_get_state();
-    CRYPTO_free_ex_index(0, -1);
     FuzzerSetRand();
 
     return 1;
