fdc7d2199f321c6253a5c5798c91fd59e15582fd
Theo Buehler <tb@openbsd.org>
2023-07-18 11:29:21 +0200

    Remove outdated parts of driver.c
    Suggested by @vanhauser-thc

--- driver.c
+++ driver.c
@@ -13,8 +13,6 @@
 #include <openssl/opensslconf.h>
 #include "fuzzer.h"
 
-#ifndef OPENSSL_NO_FUZZ_LIBFUZZER
-
 int LLVMFuzzerInitialize(int *argc, char ***argv);
 int LLVMFuzzerTestOneInput(const uint8_t *buf, size_t len);
 
@@ -27,29 +25,3 @@ int LLVMFuzzerTestOneInput(const uint8_t *buf, size_t len)
 {
     return FuzzerTestOneInput(buf, len);
 }
-
-#elif !defined(OPENSSL_NO_FUZZ_AFL)
-
-#define BUF_SIZE 65536
-
-int main(int argc, char** argv)
-{
-    FuzzerInitialize(&argc, &argv);
-
-    while (__AFL_LOOP(10000)) {
-        uint8_t *buf = malloc(BUF_SIZE);
-        size_t size = read(0, buf, BUF_SIZE);
-
-        FuzzerTestOneInput(buf, size);
-        free(buf);
-    }
-
-    FuzzerCleanup();
-    return 0;
-}
-
-#else
-
-#error "Unsupported fuzzer"
-
-#endif
